7a4169c32496add61ce737abcd13199b2722dafc
BarnetteME1 <BarnetteME1@gmail.com>
2016-06-28 20:40:15 -0400

    scraper functioning better

--- indeed/spiders/indeed_spider.py
+++ indeed/spiders/indeed_spider.py
@@ -6,16 +6,16 @@ class IndeedSpider(scrapy.Spider):
     name = "indeed"
     allowed_domains = ['www.indeed.com']
     start_urls = [
-        'http://www.indeed.com/jobs?q=&l=19103&radius=50'
+        'http://www.indeed.com/jobs?as_and=&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=50&l=19103&fromage=30&limit=10&sort=&psf=advsrch'
         ]
 
     def parse(self, response):
         ij = scrapy.Selector(response)
-        jobs = ij.xpath("//div[@class='row  result' or @class-'row sjlast result']")
+        jobs = ij.xpath("//div[@class='row  result']")
         openings = []
-
-        for job in jobs:
+        job_list = jobs.xpath('a[@target="_blank"]/text()').extract()
+        for job in job_list:
             position = IndeedItem()
-            position['jobs'] = jobs.xpath('a[@target="_blank"]/text()').extract()
+            position['jobs'] = job
             openings.append(position)
-        print("Openings: ", openings)
+        return openings
