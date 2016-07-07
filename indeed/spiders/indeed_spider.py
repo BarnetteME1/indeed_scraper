bd29c7f6318b1c1cccb4ac422d55b6039c1702c0
BarnetteME1 <BarnetteME1@gmail.com>
2016-07-06 21:58:07 -0400

    better

--- indeed/spiders/indeed_spider.py
+++ indeed/spiders/indeed_spider.py
@@ -8,13 +8,18 @@ class IndeedSpider(scrapy.Spider):
     start_urls = [
         'http://www.indeed.com/jobs?as_and=&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=50&l=19103&fromage=30&limit=10&sort=&psf=advsrch'
         ]
+    delimiter = ','
 
     def parse(self, response):
-
         for sel in response.xpath("//div[contains(@class, 'row ')]"):
-            position = IndeedItem()
-            position['jobs'] = sel.xpath('//a[contains(@data-tn-element, "jobTitle")]/text()').extract()
-            position['city'] = sel.xpath('//span[@class="location"]/text()').extract()
-            position['company'] = sel.xpath('//span[@class="company"]/text()').extract()
-
-            return position
+            items = []
+            jobs = sel.xpath('//a[contains(@data-tn-element, "jobTitle")]/text()').extract()
+            city = sel.xpath('//span[@class="location"]/text()').extract()
+            company = sel.xpath('//span[@class="company"]/text()').extract()
+            for j, c, co in zip(jobs, city, company):
+                position = IndeedItem()
+                position['jobs'] = j.strip()
+                position['city'] = c.strip()
+                position['company'] = co.strip
+                items.append(position)
+            return items
