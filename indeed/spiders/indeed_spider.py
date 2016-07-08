bbacd974d4b433ef1fbfbcb4f8c1014cede4ecf2
BarnetteME1 <BarnetteME1@gmail.com>
2016-07-07 21:41:34 -0400

    TypeError needs fixing

--- indeed/spiders/indeed_spider.py
+++ indeed/spiders/indeed_spider.py
@@ -1,14 +1,16 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from indeed.items import IndeedItem
 
 
-class IndeedSpider(scrapy.Spider):
+class IndeedSpider(CrawlSpider):
     name = "indeed"
     allowed_domains = ['www.indeed.com']
     start_urls = [
         'http://www.indeed.com/jobs?as_and=&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=50&l=19103&fromage=30&limit=10&sort=&psf=advsrch'
         ]
-    delimiter = ','
+    rules = (Rule(LinkExtractor(allow=(), restrict_xpaths=("//span[@class='np']",)), callback="parse", follow= True))
 
     def parse(self, response):
         for sel in response.xpath("//div[contains(@class, 'row ')]"):
@@ -20,6 +22,6 @@ class IndeedSpider(scrapy.Spider):
                 position = IndeedItem()
                 position['jobs'] = j.strip()
                 position['city'] = c.strip()
-                position['company'] = co.strip
+                position['company'] = co.strip()
                 items.append(position)
-            return items
+            return position
