2e0e2a3226a950f9bd59000e3ee923dc654bbccb
BarnetteME1 <BarnetteME1@gmail.com>
2016-07-13 20:46:49 -0400

    what to do?

--- indeed/spiders/indeed_spider.py
+++ indeed/spiders/indeed_spider.py
@@ -4,13 +4,17 @@ from scrapy.linkextractors import LinkExtractor
 from indeed.items import IndeedItem
 
 
-class IndeedSpider(CrawlSpider):
+class IndeedSpider(scrapy.Spider):
     name = "indeed"
     allowed_domains = ['www.indeed.com']
     start_urls = [
         'http://www.indeed.com/jobs?as_and=&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=50&l=19103&fromage=30&limit=10&sort=&psf=advsrch'
         ]
-    rules = (Rule(LinkExtractor(allow=('http://www.indeed.com/jobs?q=&l=19103&radius=50&fromage=30&start=.*'))), )
+    rules = (
+    Rule(LinkExtractor(allow=('http://www.indeed.com/jobs?as_and=&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=50&l=19103&fromage=30&limit=10&sort=&psf=advsrch', 'http://www.indeed.com/jobs?q=&l=19103&radius=50&fromage=30&start=.*'))),
+    Rule(LinkExtractor(allow=('http://www.indeed.com/jobs?q=&l=19103&radius=50&fromage=30&start=.*'))),
+    )
+
 
     def parse_item(self, response):
         for sel in response.xpath("//div[contains(@class, 'row ')]"):
